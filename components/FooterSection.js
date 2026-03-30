3bcb5de63d0ddc60cdced80a15031da5a82461ad
M Aun Rasheed <greataun786@gmail.com>
2026-03-30 21:37:03 +0500

    feat;made footer section responsive

--- components/FooterSection.js
+++ components/FooterSection.js
@@ -5,18 +5,18 @@ import { Map, Phone } from 'lucide-react'
 const FooterSection = () => {
     return (
         <>
-            <section className='absolute bottom-0 h-screen w-full'>
+            <section className='absolute bottom-0 min-h-screen h-fit py-10 w-full'>
                 <img
                     src='../assets/footerSectionBg.webp'
                     alt='hero'
                     className='absolute inset-0 w-full h-full object-cover'
                 />
 
-                <div className='absolute inset-0 bg-linear-to-b from-black/50 via-black/70 to-mist-900'></div>
+                <div className='absolute inset-0 bg-mist-800/80'></div>
 
-                <div className='relative z-10 flex flex-col items-center gap-7 justify-center h-full text-white'>
+                <div className='relative z-10 flex flex-col items-center gap-7 justify-center h-full text-white/80'>
 
-                    <h1 className='text-6xl font-bold uppercase text-center text-primary-light'>
+                    <h1 className='text-3xl md:text-5xl lg:text-6xl font-bold uppercase text-center text-primary-light'>
                         We ready to have you <br />
                         the best dining experiences
                     </h1>
@@ -29,7 +29,7 @@ const FooterSection = () => {
                         <img src="../assets/logo.png" alt="DMR SUSHI" className='w-12' />
                     </div>
 
-                    <div className='flex items-center justify-center gap-4'>
+                    <div className='flex items-center justify-center gap-4 px-5 flex-wrap'>
                         <button className='relative group py-1'>
                             <span className='group-hover:text-primary transition-colors duration-300 uppercase'>Instagram</span>
                             <span className='absolute bottom-0 left-0 h-[1.5px] w-0 bg-primary group-hover:w-full transition-all duration-300 ease-in-out origin-left'></span>
@@ -44,11 +44,11 @@ const FooterSection = () => {
                         </button>
                     </div>
 
-                    <div className='flex items-center justify-center gap-10'>
+                    <div className='flex items-center justify-center gap-x-10 gap-y-3 px-5 flex-wrap'>
                         <div className='flex items-center justify-center gap-4 text-primary'>
                             <span><Map /></span>
-                            <p>Jendral Sudirman Street Pahoman <br />
-                                Bandar Lampung, Lampung, 35222</p>
+                            <p className=''>Jendral Sudirman Street Pahoman
+                                Bandar Lampung, 35222</p>
                         </div>
                         <div className='flex items-center justify-center gap-4 text-primary'>
                             <span><Phone /></span>
